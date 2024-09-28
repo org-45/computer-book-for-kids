50c60f07a4f6277e3627a4711b712083371a4cf0
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2024-09-28 22:40:47 +0545

    test github action changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+  };
+  
+  module.exports = nextConfig;
+  
