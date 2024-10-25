db8e856aa975ad03bda25867d572197eeeab9d49
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2024-10-25 20:12:15 +0545

    fixed button issues

--- src/components/ChapterNavigation.tsx
+++ src/components/ChapterNavigation.tsx
@@ -45,107 +45,95 @@ export default function ChapterNavigation({ chapters }: ChapterNavigationProps) 
   };
 
   return (
-    <div className="">
-      <div className="md:hidden fixed top-4 right-4">
-        <Button onClick={toggleModal} className="text-white bg-blue-600 px-4 py-2 rounded-full shadow-lg">
-          <IoMdMenu />
-        </Button>
-      </div>
-
-      <div className="hidden md:block w-full bg-gray-100 p-4">
-        <CardContent className="flex-grow overflow-y-auto">
-          <ul className="space-y-2">
-            {currentChapters.map((chapter) => (
-              <li key={chapter.id}>
-                <Link
-                  href={`/posts/${chapter.id}`}
-                  passHref
-                  className="text-base md:text-lg text-blue-900 hover:text-blue-400 transition-colors duration-200"
-                >
-                  {chapter.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </CardContent>
-        {/* Pagination Controls */}
-        <div className="p-4">
-          <div className="flex justify-between">
-            <Button
-              variant="secondary"
-              onClick={prevPage}
-              disabled={currentPage === 1}
-              className="text-sm"
-            >
-              Previous
-            </Button>
-            <Button
-              variant="secondary"
-              onClick={nextPage}
-              disabled={currentPage === totalPages}
-              className="text-sm"
-            >
-              Next
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      {isModalOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
-          onClick={closeModal}
-        >
-          <div
-            className="bg-white w-full max-w-md h-auto max-h-screen overflow-y-auto p-6 rounded-lg relative z-60"
-            onClick={(e) => e.stopPropagation()} 
-          >
-            <div className="flex justify-between items-center mb-4">
-              <h2 className="text-lg font-bold">Chapters</h2>
-              <Button onClick={closeModal} className="text-sm">
-                Close
+      <div className="h-full">
+          <div className="md:hidden fixed top-4 right-4">
+              <Button onClick={toggleModal} className="text-white bg-blue-600 px-4 py-2 rounded-full shadow-lg">
+                  <IoMdMenu />
               </Button>
-            </div>
-
-            <CardContent className="flex-grow overflow-y-auto">
-              <ul className="space-y-2">
-                {currentChapters.map((chapter) => (
-                  <li key={chapter.id}>
-                    <Link
-                      href={`/posts/${chapter.id}`}
-                      passHref
-                      className="text-base text-blue-900 hover:text-blue-400 transition-colors duration-200"
-                    >
-                      {chapter.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </CardContent>
-
-            <div className="p-4">
-              <div className="flex justify-between">
-                <Button
-                  variant="secondary"
-                  onClick={prevPage}
-                  disabled={currentPage === 1}
-                  className="text-sm"
-                >
-                  Previous
-                </Button>
-                <Button
-                  variant="secondary"
-                  onClick={nextPage}
-                  disabled={currentPage === totalPages}
-                  className="text-sm"
-                >
-                  Next
-                </Button>
-              </div>
-            </div>
           </div>
-        </div>
-      )}
-    </div>
+
+          <div className="hidden md:block w-full bg-gray-100 p-4 h-full">
+              <CardContent className="flex-grow overflow-y-auto" style={{height:"95%"}}>
+                  <ul className="space-y-2">
+                      {currentChapters.map(chapter => (
+                          <li key={chapter.id}>
+                              <Link
+                                  href={`/posts/${chapter.id}`}
+                                  passHref
+                                  className="text-base md:text-lg text-blue-900 hover:text-blue-400 transition-colors duration-200">
+                                  {chapter.title}
+                              </Link>
+                          </li>
+                      ))}
+                  </ul>
+              </CardContent>
+              {/* Pagination Controls */}
+              <div className="p-4 relative">
+                  <div className="flex absolute justify-between inset-x-0 bottom-0">
+                      <Button variant="default" onClick={prevPage} disabled={currentPage === 1} className="text-sm">
+                          Previous
+                      </Button>
+                      <Button
+                          variant="default"
+                          onClick={nextPage}
+                          disabled={currentPage === totalPages}
+                          className="text-sm">
+                          Next
+                      </Button>
+                  </div>
+              </div>
+          </div>
+
+          {isModalOpen && (
+              <div
+                  className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 h-full"
+                  onClick={closeModal}>
+                  <div
+                      className="bg-white w-full max-w-md h-4/5 max-h-screen overflow-y-auto p-6 rounded-lg relative z-60"
+                      onClick={e => e.stopPropagation()}>
+                      <div className="flex justify-between items-center mb-4">
+                          <h2 className="text-lg font-bold">Chapters</h2>
+                          <Button onClick={closeModal} className="text-sm">
+                              Close
+                          </Button>
+                      </div>
+
+                      <CardContent className="flex-grow overflow-y-auto h-4/5">
+                          <ul className="space-y-2">
+                              {currentChapters.map(chapter => (
+                                  <li key={chapter.id}>
+                                      <Link
+                                          href={`/posts/${chapter.id}`}
+                                          passHref
+                                          className="text-base text-blue-900 hover:text-blue-400 transition-colors duration-200">
+                                          {chapter.title}
+                                      </Link>
+                                  </li>
+                              ))}
+                          </ul>
+                      </CardContent>
+
+                      <div className="p-4 relative">
+                          <div className="flex justify-between inset-x-0 bottom-0">
+                              <Button
+                                  variant="secondary"
+                                  onClick={prevPage}
+                                  disabled={currentPage === 1}
+                                  className="text-sm">
+                                  Previous
+                              </Button>
+                              <Button
+                                  variant="secondary"
+                                  onClick={nextPage}
+                                  disabled={currentPage === totalPages}
+                                  className="text-sm">
+                                  Next
+                              </Button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          )}
+      </div>
   );
 }
